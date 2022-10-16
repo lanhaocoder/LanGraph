af2e49b0d22b8e29c85d7745a4757a818a9bb1bf
Hao Lan <lanhaocode@foxmail.com>
2022-10-17 00:52:04 +0800

    [core] add main trace.dat
    add main trace.dat
    
    Signed-off-by: Hao Lan <lanhaocode@foxmail.com>

--- main.c
+++ main.c
@@ -7,58 +7,6 @@
 #include <tracefs.h>
 #include <stdlib.h>
 
-int load_cmdlines(struct tep_handle *tep)
-{
-        char *buf = NULL;
-        int r;
-
-        buf = tracefs_instance_file_read(NULL, "saved_cmdlines", NULL);
-        if (!buf)
-                return -1;
-        r = tep_parse_saved_cmdlines(tep, buf);
-        free(buf);
-        return r;
-}
-
-int load_print_strings(struct tep_handle *tep)
-{
-        char *buf = NULL;
-        int r;
-
-        buf = tracefs_instance_file_read(NULL, "printk_formats", NULL);
-        if (!buf)
-                return -1;
-        r = tep_parse_printk_formats(tep, buf);
-        free(buf);
-        return r;
-}
-
-int load_kallsyms(struct tep_handle *tep)
-{
-        char *line = NULL;
-        char *buf = NULL;
-        size_t sz = 0;
-        FILE *fp;
-        int len = 0;
-        int r;
-
-        fp = fopen("/proc/kallsyms", "r");
-        while ((r = getline(&line, &sz, fp)) >= 0) {
-                buf = realloc(buf, len + r + 1);
-                memcpy(buf + len, line, r);
-                len += r;
-        }
-        free(line);
-        fclose(fp);
-        if (!buf)
-                return -1;
-        buf[len] = 0;
-
-        r = tep_parse_kallsyms(tep, buf);
-        free(buf);
-        return r;
-}
-
 struct private_data {
 	int		cpu;
 	const char	*file;
@@ -97,22 +45,32 @@ trace_stack_handler(struct tracecmd_input *handle, struct tep_event *event,
 			break;
 
 		func = tep_find_function(event->tep, addr);
+		#if 0
 		if (func)
 			trace_seq_printf(&seq, "!=> %s (%llx)\n", func, addr);
 		else
 			trace_seq_printf(&seq, "!=> %llx\n", addr);
+		#endif
 	}
+
 	 trace_seq_terminate(&seq);
 	 trace_seq_do_printf(&seq);
 
 	return 0;
 }
 
-static int print_events(struct tracecmd_input *handle, struct tep_record *record, int cpu, void *data)
+static int print_events(struct tracecmd_input *handle,
+						    struct tep_record *record, int cpu, void *data)
 {
 	static struct trace_seq seq;
 	struct tep_handle *tep = tracecmd_get_tep(handle);
 	struct private_data *pdata = tracecmd_get_private(handle);
+	struct tep_event *event = tep_find_event_by_record(tep, record);
+
+	unsigned long long common_pid;
+	unsigned long long common_flags;
+	unsigned long long common_preempt_count;
+	unsigned long long common_type;
 
 	/* For multi handles we need this */
 	if (pdata->cpu >= 0 && pdata->cpu != record->cpu)
@@ -122,11 +80,20 @@ static int print_events(struct tracecmd_input *handle, struct tep_record *record
 		trace_seq_init(&seq);
 
 	trace_seq_reset(&seq);
+	if (tep_get_common_field_val(&seq, event, "common_pid", record, &common_pid, 1))
+		return -1;
+	if (tep_get_common_field_val(&seq, event, "common_flags", record, &common_flags, 1))
+		return -1;
+	if (tep_get_common_field_val(&seq, event, "common_preempt_count", record, &common_preempt_count, 1))
+		return -1;
+	if (tep_get_common_field_val(&seq, event, "common_type", record, &common_type, 1))
+		return -1;
 
-	trace_seq_printf(&seq, "%s: ", pdata->file);
-	tep_print_event(tep, &seq, record, "%6.1000d [%03d] %s-%d %s: %s\n",
+	trace_seq_printf(&seq, "%s: pid %lld, flags 0x%llx, preempt_count 0x%llx, type %llx : ", pdata->file, common_pid, common_flags,
+				     common_preempt_count, common_type);
+	tep_print_event(tep, &seq, record, "%6.1000d [%03d] %s-%d %s\n",
 			TEP_PRINT_TIME, TEP_PRINT_CPU, TEP_PRINT_COMM, TEP_PRINT_PID,
-			TEP_PRINT_NAME, TEP_PRINT_INFO);
+			TEP_PRINT_NAME);
 
 	trace_seq_terminate(&seq);
 	trace_seq_do_printf(&seq);
@@ -151,6 +118,7 @@ int main(int argc, char **argv)
 	struct tracecmd_input **handles = NULL;
 	const char *filter_str = NULL;
 	const char *argv0 = argv[0];
+	char *filename;
 	struct private_data *priv;
 	cpu_set_t *cpuset = NULL;
 	char *event = NULL;
@@ -179,8 +147,10 @@ int main(int argc, char **argv)
 	argc -= optind;
 	argv += optind;
 
-	if (argc == 0)
-		usage(argv0);
+	if (argc == 0) {
+		argv[i] = "trace.dat";
+		argc++;
+	}
 
 	for (i = 0; i < argc; i++) {
 		handles = realloc(handles, sizeof(*handles) * (nr_handles + 1));
@@ -204,7 +174,7 @@ int main(int argc, char **argv)
 		priv->file = argv[i];
 		priv->cpu = cpu;
 		tracecmd_set_private(handles[nr_handles], priv);
-
+#if 0
 		if (tracecmd_follow_event(handles[nr_handles], "ftrace", "user_stack", trace_stack_handler, NULL) < 0) {
 			printf("Could not follow event %s for file %s\n", event, argv[i]);
 			exit(-1);
@@ -213,7 +183,7 @@ int main(int argc, char **argv)
 			printf("Could not follow event %s for file %s\n", event, argv[i]);
 			exit(-1);
 		}
-
+#endif
 		nr_handles++;
 	}
 
